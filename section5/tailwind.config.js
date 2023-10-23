25daa5b74aeed2f07c7c80a0cd1b0306380d623c
hita-fttk <fttkmcc77@gmail.com>
2023-10-23 16:59:27 +0900

    独自の色を追加させた。

--- section5/tailwind.config.js
+++ section5/tailwind.config.js
@@ -8,7 +8,15 @@ module.exports = {
     extend: {
       colors:{
         'light-blue':colors.lightBlue,
+        fresh: {
+          100: '#D6FEC1',
+          200: '#AFFFB5',
+          300: '#EFFFFB',
+          400: '#C4FFFF',
+          500: '#AFE8FF',
+          600: '#ACFFD4'
       }
+    }
     },
     screens: {
       sm: '640px',
